5e487215a8f8a8b8cfae408076a8b19d55bd69d1
chase.chen <cjx463045792@gmail.com>
2024-03-22 10:26:59 +0800

    what the fuck

--- internal/logic/favoriterecordlogic.go
+++ internal/logic/favoriterecordlogic.go
@@ -3,11 +3,9 @@ package logic
 import (
 	"context"
 
+	"github.com/zeromicro/go-zero/core/logx"
 	"go_counter/go_counter"
 	"go_counter/internal/svc"
-	"go_counter/model/mysql"
-
-	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type FavoriteRecordLogic struct {
@@ -24,20 +22,21 @@ func NewFavoriteRecordLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Fa
 	}
 }
 
-func (l *FavoriteRecordLogic) FavoriteRecordV1(in *go_counter.FavoriteRecordRequest) (*go_counter.FavoriteRecordResponse, error) {
-	// todo: transaction
-	if _, err := l.svcCtx.FavoritesRecordModel.Insert(
-		l.ctx,
-		&mysql.FavoritesRecord{
-			UserId:    in.UserId,
-			ArticleId: in.ArticleId,
-		},
-	); err != nil {
-		logx.Errorw("FavoritesModel.Insert failed", logx.Field("err", err.Error()))
-		return &go_counter.FavoriteRecordResponse{Success: false}, nil
-	}
-
+func (l *FavoriteRecordLogic) FavoriteRecordV1(in *go_counter.FavoriteRecordRequest) (
+	*go_counter.FavoriteRecordResponse, error) {
 	// 有并发问题：
+
+	//if _, err := l.svcCtx.FavoritesRecordModel.Insert(
+	//	l.ctx,
+	//	&mysql.FavoritesRecord{
+	//		UserId:    in.UserId,
+	//		ArticleId: in.ArticleId,
+	//	},
+	//); err != nil {
+	//	logx.Errorw("FavoritesModel.Insert failed", logx.Field("err", err.Error()))
+	//	return &go_counter.FavoriteRecordResponse{Success: false}, nil
+	//}
+
 	//entry, _ := l.svcCtx.CountMetaModel.FindOneByBusinessIdTypes(l.ctx, in.ArticleId, "FAVORITE")
 	//if err != nil {
 	//	...
@@ -50,6 +49,10 @@ func (l *FavoriteRecordLogic) FavoriteRecordV1(in *go_counter.FavoriteRecordRequ
 	//)
 
 	// v1 for update
+	err := l.svcCtx.CountMetaModel.Incr(l.ctx, in.ArticleId, "FAVORITE", in.UserId)
+	if err != nil {
+		return &go_counter.FavoriteRecordResponse{Success: false}, err
+	}
 
 	return &go_counter.FavoriteRecordResponse{Success: true}, nil
 }
